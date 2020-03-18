b4df3351980bb82efd22f7c9c8166b3d08e4c467
Tomaz Gomes <tomgm1502@gmail.com>
2020-03-18 20:32:42 -0300

    update

--- alg_lin/target/debug/alg_lin.d
+++
@@ -1,1 +0,0 @@
-/home/tomaz/Área\ de\ Trabalho/Tom/Rust/alg_lin/target/debug/alg_lin: /home/tomaz/Área\ de\ Trabalho/Tom/Rust/alg_lin/src/main.rs
--- alg_lin/target/debug/deps/alg_lin-b64ce056d2ad811c.d
+++
@@ -1,5 +0,0 @@
-/home/tomaz/Área de Trabalho/Tom/Rust/alg_lin/target/debug/deps/alg_lin-b64ce056d2ad811c: src/main.rs
-
-/home/tomaz/Área de Trabalho/Tom/Rust/alg_lin/target/debug/deps/alg_lin-b64ce056d2ad811c.d: src/main.rs
-
-src/main.rs:
