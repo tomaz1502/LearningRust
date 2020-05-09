3596e394fc7dd4b81e4cef5808a3232c5b8087e5
tomaz1502 <tomgm1502@gmail.com>
2020-05-08 21:30:39 -0300

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+
+struct Variable {
+    name : String,
+    value : i32,
+    address : u32,
+}
+
+impl Variable {
+
+    fn inc(& mut self) {
+        self.value += 1;
+    }
+
+}
+
+fn main() {
+
+    let mut var : Variable = build_variable(String::from("var"), 10, 0);
+    var.inc();
+    //print_variable(var);
+
+    let other_var = Variable {
+        name : String::from("other"),
+        ..var
+    };
+
+    print_variable(other_var);
+
+}
+
+fn print_variable(var : Variable) {
+    println!("{:?}", var);
+}
+
+fn build_variable(name : String, value : i32, address : u32) -> Variable {
+    Variable {
+        name,
+        value,
+        address,
+    }
+}
+
