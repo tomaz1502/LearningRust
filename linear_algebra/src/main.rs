dcf417fab7fe90881f9d01ad55ed8a834b7e3e02
tomaz1502 <tomgm1502@gmail.com>
2020-05-17 20:01:11 -0300

    enums

--- linear_algebra/src/main.rs
+++ linear_algebra/src/main.rs
@@ -1,3 +1,27 @@
+#[allow(dead_code)]
+
+enum Vec {
+    TwoDim(i32, i32),
+    ThreeDim(i32, i32, i32),
+}
+
+impl Vec {
+    fn dot(u : Vec, v : Vec) -> Option<i32> {
+        match (u, v) {
+            (Vec::TwoDim(x1, y1), Vec::TwoDim(x2, y2)) => Some(x1 * x2 + y1 * y2),
+            (Vec::ThreeDim(x1, y1, z1), Vec::ThreeDim(x2, y2, z2)) => Some(x1 * x2 + y1 * y2 + z1 * z2),
+            (_, _) => None,
+        }
+    }
+
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let v = Vec::TwoDim(0, 1);
+    let u = Vec::TwoDim(1, 0);
+    match Vec::dot(u, v) {
+        Some(x) => println!("{}", x),
+        None    => println!("erro"),
+    }
 }
